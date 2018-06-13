e900345c5921c59c1dadb627cdf06d2cb7ce6050
Nekmo <contacto@nekmo.com>
2018-06-13 03:46:06 +0200

    Import Pokédex.

--- demo/pokedex/models.py
+++ demo/pokedex/models.py
@@ -24,28 +24,44 @@ class Habitat(models.Model):
         file = 'pokemon_habitats'
 
 
-class GrowthRates(models.Model):
+class GrowthRate(models.Model):
     identifier = models.CharField(max_length=20)
+    formula = models.TextField()
 
     class Import:
         file = 'growth_rates'
 
 
+class Region(models.Model):
+    identifier = models.CharField(max_length=30)
+    
+    class Import:
+        file = 'regions'
+
+
+class Generation(models.Model):
+    main_region = models.ForeignKey(Region, on_delete=models.PROTECT)
+    identifier = models.CharField(max_length=30)
+    
+    class Import:
+        file = 'generations'
+
+
 class Specie(models.Model):
     identifier = models.CharField(max_length=50)
-    generation = models.IntegerField()
-    evolves_from_specie = models.ForeignKey('self', on_delete=models.CASCADE)
+    generation = models.ForeignKey(Generation, on_delete=models.PROTECT)
+    evolves_from_specie = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     # evolution_chain =
     color = models.ForeignKey(Color, on_delete=models.PROTECT)
     shape = models.ForeignKey(Shape, on_delete=models.PROTECT)
-    habitat = models.ForeignKey(Habitat, on_delete=models.PROTECT)
+    habitat = models.ForeignKey(Habitat, on_delete=models.PROTECT, blank=True, null=True)
     gender_rate = models.SmallIntegerField()
-    capture_rate = models.PositiveIntegerField()
-    base_happiness = models.PositiveIntegerField()
+    capture_rate = models.PositiveSmallIntegerField()
+    base_happiness = models.PositiveSmallIntegerField()
     is_baby = models.BooleanField()
     hatch_counter = models.PositiveSmallIntegerField()
     has_gender_differences = models.BooleanField()
-    growth_rate = models.ForeignKey(GrowthRates, on_delete=models.PROTECT)
+    growth_rate = models.ForeignKey(GrowthRate, on_delete=models.PROTECT)
     forms_switchable = models.BooleanField()
     order = models.PositiveSmallIntegerField()
     conquest_order = models.PositiveSmallIntegerField(null=True, blank=True)
@@ -59,9 +75,10 @@ class Pokemon(models.Model):
     specie = models.ForeignKey(Specie, on_delete=models.CASCADE)
     height = models.PositiveSmallIntegerField()
     weight = models.PositiveSmallIntegerField()
-    base_experiencie = models.PositiveSmallIntegerField()
+    base_experience = models.PositiveSmallIntegerField()
     order = models.PositiveSmallIntegerField()
     is_default = models.BooleanField()
 
     class Import:
         file = 'pokemon'
+        fields_map = {'specie': 'species_id'}
