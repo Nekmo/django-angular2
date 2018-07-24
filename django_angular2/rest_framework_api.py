ee5fcd56707910c593f19ad3d3a3adf5a446d0a4
nekmo <contacto@nekmo.com>
2018-07-24 15:05:51 +0200

    Crear desde nmap.

--- django_angular2/rest_framework_api.py
+++ django_angular2/rest_framework_api.py
@@ -1,3 +1,4 @@
+import inspect
 from itertools import chain
 
 from django.conf import settings
@@ -13,13 +14,19 @@ def get_views(resolv, pattern=()):
             for sub in get_views(item, pattern + (resolv.pattern,)):
                 yield sub
     elif isinstance(resolv, URLPattern):
-        yield import_string(resolv.lookup_str), pattern + (resolv.pattern,)
+        try:
+            mod = import_string(resolv.lookup_str)
+        except ModuleNotFoundError:
+            pass
+        else:
+            yield mod, pattern + (resolv.pattern,)
 
 
 def get_api_views():
     patterns = getattr(import_string(settings.ROOT_URLCONF), 'urlpatterns')
     views = set(list(chain(*[get_views(pattern) for pattern in patterns])))
     views = [view for view in views
-             if issubclass(view[0], GenericViewSet) and '(?P<format>[a-z0-9]+)' not in view[1][-1]._regex and \
+             if inspect.isclass(view[0]) and issubclass(view[0], GenericViewSet) and \
+                '(?P<format>[a-z0-9]+)' not in view[1][-1]._regex and \
                 '(?P<pk>[^/.]+)' not in view[1][-1]._regex]
     return views
